fc70d869dc89c8050834e3dbb9acc157562012a9
zouxu9203 <zouxu9203@163.com>
2019-08-15 15:09:33 +0800

    数据库的保存、添加、修改、删除

--- db_demo.py
+++ db_demo.py
@@ -27,6 +27,10 @@ class Role(db.Model):
 
     users = db.relationship("User", backref='role')
 
+    def __repr__(self):
+        '''定义之后，可以让显示对象的时候更直观'''
+        return "Role object: name=%" %self.name
+
 
 # 表名的常见规范
 # ihome --> ih_user  数据库名缩写_表名
@@ -38,10 +42,15 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True) # 整型的主键， 会默认设置为自增主键
     name = db.Column(db.String(64), unique=True, index=True)
-    email = db.Column(db.String(64),unique=True)
-    password = db.Column(db.String(64))
+    email = db.Column(db.String(128), unique=True)
+    password = db.Column(db.String(128))
     # 设置外键
     role_id = db.Column(db.Integer, db.ForeignKey("tbl_roles.id"))
+
+    def __repr__(self):
+        '''定义之后，可以让显示对象的时候更直观'''
+        return "User object: name=%" %self.name
+
 
 
 if __name__ == '__main__':
@@ -50,3 +59,22 @@ if __name__ == '__main__':
 
     # 创建所有的表
     db.create_all()
+
+    # 创建对象
+    role1 = Role(name="admin")
+    # session记录对象任务
+    db.session.add(role1)
+    # 提交任务到数据库中
+    db.session.commit()
+
+    role2 = Role(name="stuff")
+    db.session.add(role2)
+    db.session.commit()
+
+    us1 = User(name='wang', email='wang@163.com', password='123456', role_id=role1.id)
+    us2 = User(name='zhang', email='zhang@189.com', password='201512', role_id=role2.id)
+    us3 = User(name='chen', email='chen@126.com', password='987654', role_id=role2.id)
+    us4 = User(name='zhou', email='zhou@163.com', password='456789', role_id=role1.id)
+
+    db.session.add_all([us1, us3, us2, us4]) # 一次添加多条数据
+    db.session.commit()
